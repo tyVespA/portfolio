98b75e97f664b144dd55c77c1a133472c5f87a46
tyVespA <migliavacca.marco08@gmail.com>
2023-03-22 14:41:43 +0100

    Add dummy content

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <p>Navbar</p>
+      <ThemeToggle />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div>Main</div>
+      <h1>Main</h1>
     </>
   );
 }
