40c54f648c0f5befe79091641b62115e3fbbc13f
tyVespA <migliavacca.marco08@gmail.com>
2023-03-24 12:30:47 +0100

    Add fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ import Navbar from "@/components/Navbar";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Questrial&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
