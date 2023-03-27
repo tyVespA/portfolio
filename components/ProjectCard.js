9cb5493a896869e2ebb129c2711d6c1018edb179
tyVespA <migliavacca.marco08@gmail.com>
2023-03-27 13:37:27 +0200

    Add animation on hover

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,16 +6,25 @@ import styles from "../styles/ProjectCard.module.css";
 export default function ProjectCard({ src, title, description, href }) {
   return (
     <div>
-      <Image
-        src={src}
-        width={500}
-        height={300}
-        className={styles.imageThumbnail}
-        style={{ objectFit: "contain" }}
-      />
+      <Link href={href}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={src}
+            width={500}
+            height={300}
+            className={styles.imageThumbnail}
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </Link>
       <h3 style={{ marginBottom: 10 }}>{title}</h3>
       <p style={{ marginBottom: 10 }}>{description}</p>
-      <Link href={href}>Vai al progetto</Link>
+      <Link
+        href={href}
+        className={`${styles.projectLink} ${"underlineOnHover"}`}
+      >
+        Vai al progetto
+      </Link>
     </div>
   );
 }
