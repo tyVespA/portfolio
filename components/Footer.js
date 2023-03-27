d29161279a03aaf5a059670393d8642baa54799b
tyVespA <migliavacca.marco08@gmail.com>
2023-03-27 11:41:07 +0200

    Add animation on hover

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "../styles/Footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <p>Footer Logo</p>
       <div>
-        <p>Github</p>
+        <Link href="https://github.com/tyVespA" className="underlineOnHover">
+          Github
+        </Link>
         <p>Contatti</p>
       </div>
     </div>
