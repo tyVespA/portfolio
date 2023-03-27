11ce78b947763ba59950e564aae6d70eda41cfb8
tyVespA <migliavacca.marco08@gmail.com>
2023-03-27 15:03:31 +0200

    Add underline animation when hovering on the image

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/ProjectCard.module.css";
 
 export default function ProjectCard({ src, title, description, href }) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div>
       <Link href={href}>
-        <div className={styles.imageWrapper}>
+        <div
+          className={styles.imageWrapper}
+          onMouseOver={() => setIsHovered(true)}
+          onMouseOut={() => setIsHovered(false)}
+        >
           <Image
             src={src}
             width={500}
@@ -21,9 +26,11 @@ export default function ProjectCard({ src, title, description, href }) {
       <p style={{ marginBottom: 10 }}>{description}</p>
       <Link
         href={href}
-        className={`${styles.projectLink} ${"underlineOnHover"}`}
+        className={`${styles.projectLink} ${"underlineOnHover"} ${
+          isHovered ? styles.underlined : ""
+        }`}
       >
-        Vai al progetto
+        Vai al progetto &gt;
       </Link>
     </div>
   );
