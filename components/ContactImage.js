466e73bbbe7b0f1f30e17922d1ffc0585cbc4aaf
tyVespA <migliavacca.marco08@gmail.com>
2023-04-01 22:38:15 +0200

    Add fade-in animation, change orbGradient timings

--- components/ContactImage.js
+++ components/ContactImage.js
@@ -3,7 +3,13 @@ import styles from "../styles/ContactImage.module.css";
 
 export default function ContactImage() {
   return (
-    <div className={styles.imageContainer}>
+    <div
+      className={styles.imageContainer}
+      data-aos="fade-in"
+      data-aos-once="true"
+      data-aos-delay="300"
+      data-aos-duration="1000"
+    >
       <div className={styles.orbOne}></div>
       <div className={styles.orbTwo}></div>
       <div className={styles.orbThree}></div>
