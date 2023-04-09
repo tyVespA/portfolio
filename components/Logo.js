66ed10d30151375237e2c1744b58d4cf45e12a47
tyVespA <migliavacca.marco08@gmail.com>
2023-04-09 16:26:37 +0200

    Add logo images, create variable Logo component

---
+++ components/Logo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/pages/_app";
+import Link from "next/link";
+
+export default function Logo() {
+  const { activeTheme } = useContext(ThemeContext);
+  const logoColor = activeTheme === "light" ? "dark" : "light";
+  return (
+    <>
+      <Link href="/">
+        <Image
+          src={`/images/logo-${logoColor}.svg`}
+          width={626 / 14}
+          height={538 / 14}
+          alt="Logo"
+        />
+      </Link>
+    </>
+  );
+}
