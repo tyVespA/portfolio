4e064b2926695b0d15bc2d77962b484355fec46f
tyVespA <migliavacca.marco08@gmail.com>
2023-04-27 06:22:22 +0200

    Add Navbar and Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from "react";
 import styles from "../styles/Layout.module.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
 }
