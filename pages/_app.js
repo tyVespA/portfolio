1e8fbe775016c975e9e27b391a7b72bd021dbb6f
tyVespA <migliavacca.marco08@gmail.com>
2023-05-01 09:55:20 +0200

    Change default theme

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { useState, Context, useEffect } from "react";
 export const ThemeContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
-  const [activeTheme, setActiveTheme] = useState("light");
+  const [activeTheme, setActiveTheme] = useState("dark");
 
   const value = { activeTheme, setActiveTheme };
   return (
