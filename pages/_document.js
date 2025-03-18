34139f921d5fcab85b87b9eafee000d2e955f45a
tyVespA <migliavacca.marco08@gmail.com>
2025-03-18 11:52:47 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="it">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Questrial&display=swap"
           rel="stylesheet"
